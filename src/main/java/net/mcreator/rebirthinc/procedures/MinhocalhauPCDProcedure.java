b78b528aa80d7417c97e59dfaf3e9323b6384578
1mpdragon <isaaccaasi2006@gmail.com>
2025-09-27 17:58:05 -0300

    Atualização bloco

--- src/main/java/net/mcreator/rebirthinc/procedures/MinhocalhauPCDProcedure.java
+++ src/main/java/net/mcreator/rebirthinc/procedures/MinhocalhauPCDProcedure.java
@@ -1,39 +1,43 @@
 package net.mcreator.rebirthinc.procedures;
 
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.Mth;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
-import net.mcreator.rebirthinc.network.RebirthIncModVariables;
 import net.mcreator.rebirthinc.init.RebirthIncModBlocks;
-import net.mcreator.rebirthinc.RebirthIncMod;
+
+import java.util.Map;
 
 public class MinhocalhauPCDProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		RebirthIncModVariables.MapVariables.get(world).sx = Mth.nextDouble(RandomSource.create(), -1, 1);
-		RebirthIncModVariables.MapVariables.get(world).syncData(world);
-		RebirthIncMod.queueServerWork(20, () -> {
-			RebirthIncModVariables.MapVariables.get(world).sy = Mth.nextDouble(RandomSource.create(), -1, 1);
-			RebirthIncModVariables.MapVariables.get(world).syncData(world);
-			RebirthIncMod.queueServerWork(20, () -> {
-				RebirthIncModVariables.MapVariables.get(world).sz = Mth.nextDouble(RandomSource.create(), -1, 1);
-				RebirthIncModVariables.MapVariables.get(world).syncData(world);
-				if ((world.getBlockState(BlockPos.containing(x + RebirthIncModVariables.MapVariables.get(world).sx, y + RebirthIncModVariables.MapVariables.get(world).sy, z + RebirthIncModVariables.MapVariables.get(world).sz)))
-						.getBlock() == Blocks.REINFORCED_DEEPSLATE) {
-					world.setBlock(BlockPos.containing(x + RebirthIncModVariables.MapVariables.get(world).sx, y + RebirthIncModVariables.MapVariables.get(world).sy, z + RebirthIncModVariables.MapVariables.get(world).sz),
-							RebirthIncModBlocks.MINHOCALHAU.get().defaultBlockState(), 3);
+		Direction local_random_direction = Direction.NORTH;
+		double random_int = 0;
+		if (Mth.nextDouble(RandomSource.create(), 1, 10) < 2) {
+			if (!world.isClientSide() && world.getServer() != null)
+				world.getServer().getPlayerList().broadcastSystemMessage(Component.literal("spread"), false);
+			local_random_direction = Direction.getRandom(RandomSource.create());
+			if (!((world.getBlockState(BlockPos.containing(x + local_random_direction.getStepX(), y + local_random_direction.getStepY(), z + local_random_direction.getStepZ()))).getBlock() == Blocks.AIR)) {
+				{
+					BlockPos _bp = BlockPos.containing(x + local_random_direction.getStepX(), y + local_random_direction.getStepY(), z + local_random_direction.getStepZ());
+					BlockState _bs = RebirthIncModBlocks.MINHOCALHAU.get().defaultBlockState();
+					BlockState _bso = world.getBlockState(_bp);
+					for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+						Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
+						if (_property != null && _bs.getValue(_property) != null)
+							try {
+								_bs = _bs.setValue(_property, (Comparable) entry.getValue());
+							} catch (Exception e) {
+							}
+					}
+					world.setBlock(_bp, _bs, 3);
 				}
-				RebirthIncMod.queueServerWork(20, () -> {
-					RebirthIncModVariables.MapVariables.get(world).sx = 0;
-					RebirthIncModVariables.MapVariables.get(world).syncData(world);
-					RebirthIncModVariables.MapVariables.get(world).sy = 0;
-					RebirthIncModVariables.MapVariables.get(world).syncData(world);
-					RebirthIncModVariables.MapVariables.get(world).sz = 0;
-					RebirthIncModVariables.MapVariables.get(world).syncData(world);
-				});
-			});
-		});
+			}
+		}
 	}
 }
